0de76d07f55d2509ee57a4d6db9d4e3033529cc2
Olasehinde Omoriwo <olasehindeomoriwo@Olasehindes-MacBook-Air.local>
2022-04-15 18:39:18 +0100

    dd

---
+++ first_ex.py
@@ -0,0 +1,298 @@
+print("Hello World")
+print("Hello, World!")
+print("Hello everyone")
+if 5 > 2:
+    print("Five is greater than two!")
+if 10 > 10:
+    print("Levels don change now, I'm 10 over 10")
+x = 5
+y = "Hello, World"
+print(x, y)
+x = 10
+y = "This is a cold sobo"
+print(x)
+print(y)
+#This is a comment.
+print("Hello, World!")
+carname = ("Volvo")
+x = 50
+print(x)
+x = 5
+y = 10
+print(x + y)
+x = 5
+y = 10
+z = x + y
+print(z)
+#This is a comment
+print("Hello, World!")
+print("Hello, World!") #This is a comment
+#print("Hello, World!")
+print("Cheers, Mate!")
+#This is a comment
+#written in
+#more than just one line
+print("Hello, World")
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
+#This is a comment
+print("Python has no command for declaring variables")
+x = 5
+y = "John"
+print(x)
+print(y)
+x = 34 #x is of type int
+x = "Jolla" #x is now of str
+print(x)
+x = "Rated" #x is of type str
+x = 2 #x is now of int
+print(x)
+x = 23
+y = 3
+z = (x+y)
+print(z)
+x = 23
+y = "Jordan"
+print(x)
+print(y)
+x = 15
+y = "Sola"
+print(x, y)
+x = 15
+y = 12
+z = (x+y)
+print(z)
+x = 12
+y = "One man"
+print(x, y)
+x = 4 # x is of type int
+x = "Sola" # x is now of type str
+print(x)
+x = str(3) # x will be '3'
+y = int(3) # y will be 3
+z = float(3) # z will be 3.0
+x = 5
+y = "John"
+print(type(x));
+print(type(y))
+x = "John"
+# is the same as
+x = 'John'
+a = 4
+A = "Sally"
+#A will not overite a
+print(a)
+print(A)
+x = 12
+print(x)
+myvar = "John"
+print(myvar)
+my_var = "John"
+print(my_var)
+_my_var = "John"
+print(_my_var)
+myVar = "John"
+print(myVar)
+MYVAR = "John"
+print(MYVAR)
+myvar2 = "John"
+print(myvar2)
+myVariableName = "John"
+print(myVariableName)
+MyVariableName = "John"
+print(MyVariableName)
+my_variable_name = "John"
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+fruits = ("apple", "banana", "cherry")
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+x = "Python is awesome"
+print(x)
+x = "Python"
+y = " is"
+z = " awesome"
+print(x, y, z)
+x = "Python"
+y = " is"
+z = " awesome"
+print(x + y + z)
+x = "Python"
+y = " is not "
+z = "awesome."
+print(x+y+z)
+x = 5
+y = 10
+print(x + y)
+x = 5
+y = "John"
+print(x, y)
+x = 5
+y = "John"
+print(x, y)
+x = "awesome"
+def myfunc():
+    print("python is " + x)    
+myfunc()
+x = "awesome"
+def myfunc():
+    x = "fantastic"
+    print("Python is " + x)
+    myfunc()
+    print("Python is " + x)
+def myfunc():
+    global x
+    x = "fantastic" 
+myfunc()
+print("Python is " + x)  
+x = "awesome"
+def myfunc():
+    global x
+    x = "nice"
+myfunc()
+print("Python is " + x)
+x = 5
+print(type(x))
+x = "Hello World"
+#display x:
+print(x)
+#display the data type of x:
+print(type(x))
+x = 20
+#display x:
+print(x)
+#display the data type of x:
+print(type(x))
+x = 20.5
+#display x:
+print(x)
+#display the data type of x:
+print(type(x))
+x = 1j
+#display x:
+print(x)
+#display the data type of x:
+print(type(x))
+x = ["Apple", "banana", "cherry"]
+#display x:
+print(x)
+#display data type of x:
+print(type(x))
+x = ("Apple", "banana", "cherry")
+#display x:
+print(x)
+#display the data type of x:
+print(type(x))
+x = range(6)
+#display x:
+print(x)
+#display the data type of x:
+print(type(x))
+x = {"name" : "John", "age" :
+    36}
+#display x:
+print(x)
+#display the data type of x:
+print(type(x))
+x = {"apple", "banana", "cherry"}
+#display x:
+print(x)
+#display data type of x:
+print(type(x))
+x = frozenset({"apple", "banana", "cherry"})
+#display x:
+print(x)
+#display the data type of x:
+print(type(x))
+x = True
+#display x:
+print(x)
+#display the data type of x:
+print(type(x))
+x = b"Hello"
+#display x:
+print(x)
+#display the data type of x:
+print(type(x))
+x = bytearray(5)
+#display x:
+print(x)
+#display the data type of x:
+print(type(x))
+x = memoryview(bytes(5))
+#display x:
+print(x)
+#display the data type of x:
+print(type(x))
+x = str("Hello World")
+#display x:
+print(x)
+#display the data type of x:
+print(type(x))
+x = int(20)
+#display x:
+print(x)
+#display the data type of x:
+print(type(x))
+x = complex(1j)
+#display x:
+print(x)
+#display the data type of x:
+print(type(x))
+x = list (("apple", "banana", "cherry"))
+#display x:
+print(x)
+#display the data type of x:
+print(type(x))
+x = tuple(("apple", "banana", "cherry"))
+#display x:
+print(x)
+#display the data type of x:
+print(type(x))
+x = range(6)
+#display x:
+print(x)
+#display the data type of x:
+print(type(x))
+x = set (("apple", "banana", "cherry"))
+#display x:
+print(x)
+#display the data type of x:
+print(type(x))  
+x = frozenset(("apple", "banana", "cherry"))  
+#display x:
+print(x)
+#display the data type of x:
+print(type(x))
+x = bool(5)
+#display x:
+print(x)
+#display the data type of x:
+print(type(x))
+x = bytes(5)
+#display x:
+print(x)
+#display the data type of x:
+print(type(x))
+x = bytearray(5)
+#display x:
+print(x)
+#display the data type of x:
+print(type(x))
+x = memoryview(bytes(5))
+#display x:
+print(x)
+#display the data type of x:
+print(type(x))
