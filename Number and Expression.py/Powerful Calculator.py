c78315c36ce55bf1a3ccebbcf531a79effbfbbb4
Olasehinde Omoriwo <olasehindeomoriwo@Olasehindes-MacBook-Air.local>
2022-04-18 11:25:15 +0100

    Playing with Numbers

--- Number and Expression.py/Powerful Calculator.py
+++ Number and Expression.py/Powerful Calculator.py
@@ -1,3 +1,6 @@
+from re import X
+
+
 x = 2 + 2
 print(x)
 x = 53672 + 235253
@@ -6,3 +9,9 @@ x = 1 / 2
 print(x)
 x = 1 / 1
 print(x)
+x = 1 // 2
+print(x)
+x = 1 // 1
+print(x)
+x = 5.0 // 3.4
+print(X)
---
+++ Number and Expression.py/The Remainder Modulus Operator.py
@@ -0,0 +1,29 @@
+from re import X
+
+
+x = 10 // 3
+print(X)
+x = 10 % 3
+print(x)
+x = 9 // 3
+print(x)
+x = 9 % 3
+print(x)
+2.75 % 0.5
+print(X)
+x = 10 % 3
+print(x)
+x = 10 % -3
+print(x)
+x = -10 % 3
+print(x)
+x = -10 % -3
+print(x)
+x = 10 // 3
+print(x)
+x = 10 // -3
+print(x)
+x = -10 // 3
+print(x)
+x = -10 // -3
+print(x)
