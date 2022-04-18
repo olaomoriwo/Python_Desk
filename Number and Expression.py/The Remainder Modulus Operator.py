1f44114d1cb29dafcf969735baaa07671de230ec
Olasehinde Omoriwo <olasehindeomoriwo@Olasehindes-MacBook-Air.local>
2022-04-18 13:30:05 +0100

    Input

--- Number and Expression.py/The Remainder Modulus Operator.py
+++ Number and Expression.py/The Remainder Modulus Operator.py
@@ -27,3 +27,9 @@ x = -10 // 3
 print(x)
 x = -10 // -3
 print(x)
+x = 2 ** 3
+print(x)
+x = -3 ** 2
+print(x)
+x = (-3) ** 2
+print(x)
---
+++ Variables.py/Getting Input from the User.py
@@ -0,0 +1,7 @@
+input("The meaning of life: ")
+'42'
+x = input("x: ")
+x: 34
+y = input("y: ")
+y: 42
+print(int(x) * int(y))
