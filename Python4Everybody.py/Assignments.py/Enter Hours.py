84c99c9a9394b6fa29bebc726a4081dfcadfe7c0
Olasehinde Omoriwo <olasehindeomoriwo@Olasehindes-MacBook-Air.local>
2022-05-09 23:43:28 +0100

    Read MD  modifications

--- Python4Everybody.py/Assignments.py/Enter Hours.py
+++ Python4Everybody.py/Assignments.py/Enter Hours.py
@@ -1,17 +1,17 @@
-sh = input("Enter Hours: ")
-sr = input("Enter Rate: ")
+sd = input("Enter Hours: ")
+ls = input("Enter Rate: ")
 try:
-    fh = float(sh)
-    fr = float(sr)
+    gg = float(sd)
+    ts = float(ls)
 except: 
     print("Error, please enter numeric input")
 quit()
-print(fh, fr)
-if fh > 40:
-    reg = fr * fh
-    otp = (fh - 40.0) * (fr * 0.5)
+print(gg, ts)
+if gg > 40:
+    reg = ts * gg
+    otp = (gg - 40.0) * (ts * 0.5)
     xp = ref + otp
 else:
-    xp = fh * fr
+    xp = gg * ts
 print("Pay:",xp)
     
--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # Python_Desk
-Hello, this is a documentation of my journey to and through Python. 
+Hello there ;-)
+Olasehinde here ☺️
+This a documentation of my journey to and through Python. 
 Here, you will find excercise, assignments, practice and more demonstrative usecase of my coding understanding. 
+Happy to have you here
 :)
